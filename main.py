925b2143bb42818a8df93a08bbcb4c89d8113a99
Ollie Hooper <35010178+Ollie-Hooper@users.noreply.github.com>
2023-02-13 09:29:53 +0000

    Added timing over whole process

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 from drone_atc.agent import Drone
 from drone_atc.scheduler import MPModelManager, Model
 
@@ -8,7 +10,10 @@ def main():
     n_steps = 1
 
     with MPModelManager(Model, n_processes, Drone, agents_per_process, n_steps) as model:
+        ts = time.time()
         model.go()
+        te = time.time()
+        print(te-ts)
 
 
 if __name__ == "__main__":
