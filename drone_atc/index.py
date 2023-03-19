416031c36f8dc37483dfe6ee8ca44de3a9510679
Ollie Hooper <35010178+Ollie-Hooper@users.noreply.github.com>
2023-03-19 08:28:16 -0300

    Uncommented indexes

--- drone_atc/index.py
+++ drone_atc/index.py
@@ -1,9 +1,9 @@
 from abc import ABC, abstractmethod
 
 import numpy as np
-# import shapely
+import shapely
 
-# from sklearn.neighbors import KDTree, BallTree as SKLearnBallTree
+from sklearn.neighbors import KDTree, BallTree as SKLearnBallTree
 
 from numpy.typing import ArrayLike
 
@@ -48,44 +48,44 @@ class BruteForceIndex(BaseIndex):
         return in_range
 
 
-# class RTree(BaseIndex):
-#     def __init__(self, n_agents):
-#         self.tree = None
-#         self.points = []
-#
-#     def update(self, agents: ArrayLike):
-#         self.points = [shapely.Point(r[0], r[1]) for r in agents]
-#         self.tree = shapely.STRtree(self.points)
-#
-#     def agents_in_range(self, agent: int, r: float) -> np.ndarray:
-#         query_geom = self.points[agent].buffer(r)
-#         in_range = self.tree.query(query_geom)
-#         return in_range
+class RTree(BaseIndex):
+    def __init__(self, n_agents):
+        self.tree = None
+        self.points = []
+
+    def update(self, agents: ArrayLike):
+        self.points = [shapely.Point(r[0], r[1]) for r in agents]
+        self.tree = shapely.STRtree(self.points)
+
+    def agents_in_range(self, agent: int, r: float) -> np.ndarray:
+        query_geom = self.points[agent].buffer(r)
+        in_range = self.tree.query(query_geom)
+        return in_range
 
 
-# class Quadtree(BaseIndex):
-#     def __init__(self, n_agents):
-#         self.tree = None
-#         self.points = []
-#
-#     def update(self, agents: ArrayLike):
-#         self.points = agents
-#         self.tree = KDTree(self.points)
-#
-#     def agents_in_range(self, agent: int, r: float) -> np.ndarray:
-#         in_range = self.tree.query_radius(self.points[agent].reshape(1, -1), r)[0]
-#         return in_range
+class Quadtree(BaseIndex):
+    def __init__(self, n_agents):
+        self.tree = None
+        self.points = []
+
+    def update(self, agents: ArrayLike):
+        self.points = agents
+        self.tree = KDTree(self.points)
+
+    def agents_in_range(self, agent: int, r: float) -> np.ndarray:
+        in_range = self.tree.query_radius(self.points[agent].reshape(1, -1), r)[0]
+        return in_range
 
 
-# class BallTree(BaseIndex):
-#     def __init__(self, n_agents):
-#         self.tree = None
-#         self.points = []
-#
-#     def update(self, agents: ArrayLike):
-#         self.points = agents
-#         self.tree = SKLearnBallTree(self.points)
-#
-#     def agents_in_range(self, agent: int, r: float) -> np.ndarray:
-#         in_range = self.tree.query_radius(self.points[agent].reshape(1, -1), r)[0]
-#         return in_range
+class BallTree(BaseIndex):
+    def __init__(self, n_agents):
+        self.tree = None
+        self.points = []
+
+    def update(self, agents: ArrayLike):
+        self.points = agents
+        self.tree = SKLearnBallTree(self.points)
+
+    def agents_in_range(self, agent: int, r: float) -> np.ndarray:
+        in_range = self.tree.query_radius(self.points[agent].reshape(1, -1), r)[0]
+        return in_range
